59f258e7d77f5ec4974a013c6bf7f9851e3fe381
Rasmus Hummelmose <rasmus@hummelmose.dk>
2012-06-29 12:30:11 +0200

    Initial Commit

---
+++ ETA/ETA.h
@@ -0,0 +1,13 @@
+//
+//  ETA.h
+//  ETA
+//
+//  Created by Rasmus Hummelmose on 29/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ETA : NSObject
+
+@end
---
+++ ETA/ETA.m
@@ -0,0 +1,13 @@
+//
+//  ETA.m
+//  ETA
+//
+//  Created by Rasmus Hummelmose on 29/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ETA.h"
+
+@implementation ETA
+
+@end
