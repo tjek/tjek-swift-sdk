ae3ae5a1b00f656ccd9824440cdf5f56161bb827
Laurie Hufford <lauriehufford@gmail.com>
2014-02-25 16:22:15 +0100

    Update example app

--- Examples/ETA-SDK iOS Example/ETA-SDK iOS Example/ETA_APIKeyAndSecret.h
+++ Examples/ETA-SDK iOS Example/ETA-SDK iOS Example/ETA_APIKeyAndSecret.h
@@ -1,5 +1,8 @@
+
+// you need to change these values by registering your app at https://etilbudsavis.dk/developers/
+
 static NSString* const ETA_APIKey = @"00hivl9ae8h73szhvsc8n6yg1m3f7byy";
 static NSString* const ETA_APISecret = @"00hivl9ae8xqmgw8a50nctcrr2qgtbds";
 
-static NSString* const ETA_DummyUserEmail = @"lh@etilbudsavis.dk";
-static NSString* const ETA_DummyUserPassword = @"lhlhlh";
+static NSString* const ETA_DummyUserEmail = @"test_user@etilbudsavis.dk";
+static NSString* const ETA_DummyUserPassword = @"testtesttest";
