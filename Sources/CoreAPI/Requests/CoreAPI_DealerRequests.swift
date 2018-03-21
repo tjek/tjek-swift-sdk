07329155c7839de8e31e844c7849525d5094cb3a
Laurie Hufford <lauriehufford@gmail.com>
2018-03-21 21:51:59 +0100

    Split getDealers request into two

--- Sources/CoreAPI/Requests/CoreAPI_DealerRequests.swift
+++ Sources/CoreAPI/Requests/CoreAPI_DealerRequests.swift
@@ -22,28 +22,29 @@ extension CoreAPI.Requests {
                      requiresAuth: true)
     }
     
-    // TODO: include a LocationQuery. Sort order? Paginatate only when no ids?
+    public static func getDealers(pagination: PaginatedQuery = PaginatedQuery(count: 24)) -> CoreAPI.Request<[CoreAPI.Dealer]> {
+        
+        var params: [String: String] = [:]
+        params.merge(pagination.requestParams) { (_, new) in new }
+        
+        return .init(path: "/v2/dealers",
+                     method: .GET,
+                     requiresAuth: true,
+                     parameters: params)
+    }
+    
     /**
-     A request to fetch `Dealer` objects.
+     A request to fetch a list of `Dealer` objects.
      
-     - parameter dealerIds: An optional array of dealer ids to fetch. If included (and not empty) the resulting list of dealers will be limited to those with the specified ids.
+     - parameter dealerIds: An array of dealer ids to fetch. The resulting list of dealers will be limited to those with the specified ids.
      - parameter pagination: Use to specify how many objects to fetch, and with what start cursor (offset). Defaults to the first 100 objects.
      */
-    public static func getDealers(withIds dealerIds: [CoreAPI.Dealer.Identifier]? = nil, pagination: PaginatedQuery = PaginatedQuery(count: 100)) -> CoreAPI.Request<[CoreAPI.Dealer]> {
-        
-        // If we have only a single dealer Id, use the 'getDealer' request, and map the result into an array
-        if let firstDealerId = dealerIds?.first, dealerIds?.count == 1 {
-            return .init(request: self.getDealer(withId: firstDealerId)) {
-                $0.mapValue({ [$0] })
-            }
-        }
+    public static func getDealers(withIds dealerIds: [CoreAPI.Dealer.Identifier], pagination: PaginatedQuery = PaginatedQuery(count: 100)) -> CoreAPI.Request<[CoreAPI.Dealer]> {
         
         var params: [String: String] = [:]
         params.merge(pagination.requestParams) { (_, new) in new }
 
-        if let dealerIds = dealerIds {
-            params["dealer_ids"] = dealerIds.map({ $0.rawValue }).joined(separator: ",")
-        }
+        params["dealer_ids"] = dealerIds.map({ $0.rawValue }).joined(separator: ",")
         
         return .init(path: "/v2/dealers",
                      method: .GET,
