d04b02723f4cecd7d1f5ca8083116528e40631b8
Laurie Hufford <lauriehufford@gmail.com>
2018-08-06 22:18:02 +0200

    Add tests and documentation to the Event object

--- Sources/EventsTracker/Event.swift
+++ Sources/EventsTracker/Event.swift
@@ -9,17 +9,39 @@
 
 import Foundation
 
+/**
+ An Event defines a package of data that can be sent to the EventsTracker. There are some core properties that are required, and any additional metadata is added to the `payload`.
+ */
 public struct Event {
+    /// The Identifier type of an Event, using `GenericIdentifier` make sure we dont mix types.
     public typealias Identifier = GenericIdentifier<Event>
+    
+    /// An Event's payload is defined as a dictionary of JSONValue types.
     public typealias PayloadType = [String: JSONValue]
     
+    /// The unique identifier of the event. A UUID string.
     public let id: Identifier
+    
+    /// The version of the event. If the format of the event ever changes, this may increase. It is used to choose where to send the event, and by the server to decide how to process the event.
     public let version: Int
+    
+    /// The date the event was triggered.
     public let timestamp: Date
+    
+    /// The type identifier of the event. There are a set of pre-defined constants that can be used here. For the server to be able to parse the event, the type & payload must be consistent.
     public let type: Int
     
+    /// The metadata of the event. A dictionary of JSONValue pairs, with String keys.
     public private(set) var payload: PayloadType
     
+    /**
+     Creates a new Event.
+     - parameter id: The unique identifier of the event. Defaults to a newly generated id.
+     - parameter version: The version number if the event's format. Defaults to 2
+     - parameter timestamp: The date that the event was triggered. Defaults to the current date.
+     - parameter type: The type identifier of the event. You must provide this value.
+     - parameter payload: The `payload` metadata to include with the event. Defaults to empty.
+     */
     public init(id: Identifier = Identifier.generate(), version: Int = 2, timestamp: Date = Date(), type: Int, payload: PayloadType = [:]) {
         self.id = id
         self.version = version
@@ -28,23 +50,29 @@ public struct Event {
         self.payload = payload
     }
     
-    /// Will merge the newPayload into the old one. New values for existing keys will be used (so be careful!)
-    mutating func mergePayload(_ newPayload: PayloadType) {
+    /**
+     Merges a new payload into the current one. New values for existing keys will override the old ones (so be careful!).
+     - parameter newPayload: The dictionary of JSONValues to merge into the event's payload.
+     */
+    public mutating func mergePayload(_ newPayload: PayloadType) {
         self.payload.merge(newPayload) { (_, new) in new }
+    }
+    
+}
+
+// MARK: - Equatable
+
+extension Event: Equatable {
+    public static func == (lhs: Event, rhs: Event) -> Bool {
+        return lhs.id == rhs.id
+            && lhs.version == rhs.version
+            && lhs.timestamp.eventTimestamp == rhs.timestamp.eventTimestamp
+            && lhs.type == rhs.type
+            && lhs.payload == rhs.payload
     }
 }
 
 // MARK: - Codable
-
-extension Date {
-    var eventTimestamp: Int {
-        return Int(timeIntervalSince1970)
-    }
-    
-    init(eventTimestamp: Int) {
-        self = .init(timeIntervalSince1970: TimeInterval(eventTimestamp))
-    }
-}
 
 extension Event: Codable {
     
@@ -53,6 +81,7 @@ extension Event: Codable {
         case version
         case type
         case timestamp
+        /// This case defines all unknown payload keys.
         case payload(key: String)
         
         var stringValue: String {
@@ -87,12 +116,12 @@ extension Event: Codable {
         let type = try container.decode(Int.self, forKey: .type)
         let timestamp = try container.decode(Int.self, forKey: .timestamp)
         
-        let payload: [String: JSONValue] = container.allKeys.reduce(into: [:], { (result, key) in
+        let payload: [String: JSONValue] = try container.allKeys.reduce(into: [:], { (result, key) in
             // only decode non-known payload keys
             guard case .payload = key else { return }
             
-            // decode the key as a JSONValue. Skip if nil.
-            guard let value = try? container.decode(JSONValue.self, forKey: key) else { return }
+            // decode the key as a JSONValue.
+            let value = try container.decode(JSONValue.self, forKey: key)
             
             result[key.stringValue] = value
         })
@@ -111,5 +140,17 @@ extension Event: Codable {
         try self.payload.forEach { key, value in
             try container.encode(value, forKey: .payload(key: key))
         }
+    }
+}
+
+// MARK: Event Dates
+
+extension Date {
+    var eventTimestamp: Int {
+        return Int(timeIntervalSince1970)
+    }
+    
+    init(eventTimestamp: Int) {
+        self = .init(timeIntervalSince1970: TimeInterval(eventTimestamp))
     }
 }
---
+++ Tests/EventTests.swift
@@ -0,0 +1,146 @@
+//
+//  ┌────┬─┐         ┌─────┐
+//  │  ──┤ └─┬───┬───┤  ┌──┼─┬─┬───┐
+//  ├──  │ ╷ │ · │ · │  ╵  │ ╵ │ ╷ │
+//  └────┴─┴─┴───┤ ┌─┴─────┴───┴─┴─┘
+//               └─┘
+//
+//  Copyright (c) 2018 ShopGun. All rights reserved.
+
+import XCTest
+@testable import ShopGunSDK
+
+class EventTests: XCTestCase {
+
+    func testInit() {
+
+        // 1. given
+        let id = Event.Identifier(rawValue: "abc123")
+        let version = 4
+        let type: Int = 999
+        let date = Date()
+        let payload: [String: JSONValue] = ["a": .string("abc"),
+                                            "b": .int(1),
+                                            "c": .number(1.5),
+                                            "d": .array([.string("x"), .string("y"), .string("z")]),
+                                            "e": .object(["m": .int(3)]),
+                                            "f": .bool(true),
+                                            "g": .null]
+        
+        // 2. when
+        let defaultEvent = Event(type: type)
+        let fullEvent = Event(id: id, version: version, timestamp: date, type: type, payload: payload)
+        
+        // 3. then
+        XCTAssertEqual(defaultEvent.type, type)
+        XCTAssertEqual(defaultEvent.version, 2)
+        XCTAssertEqual(defaultEvent.payload, [:])
+        XCTAssert(defaultEvent.timestamp >= date)
+        XCTAssert(defaultEvent.timestamp <= Date())
+        
+        XCTAssertEqual(fullEvent.type, type)
+        XCTAssertEqual(fullEvent.version, version)
+        XCTAssertEqual(fullEvent.type, type)
+        XCTAssertEqual(fullEvent.timestamp, date)
+        XCTAssertEqual(fullEvent.payload, payload)
+    }
+
+    
+    func testPayload() {
+        // 1. given
+        var baseEvent = Event(type: 999)
+        
+        // 2. when
+        baseEvent.mergePayload(["a": .int(1),
+                                "b": .string("foo")])
+        
+        baseEvent.mergePayload(["a": .int(2),
+                                "b": .string("bar"),
+                                "c": .bool(true)])
+        // 3. then
+        XCTAssertEqual(baseEvent.payload["a"], .int(2))
+        XCTAssertEqual(baseEvent.payload["b"], .string("bar"))
+        XCTAssertEqual(baseEvent.payload["c"], .bool(true))
+    }
+    
+    func testEventTimestamp() {
+        // 1. given
+        let date1 = Date(timeIntervalSince1970: 123456)
+        let date2 = Date(timeIntervalSince1970: -123456)
+        let date3 = Date()
+        let date4 = Date(eventTimestamp: 123456)
+        
+        // 3. then
+        XCTAssertEqual(date1.eventTimestamp, 123456)
+        XCTAssertEqual(date2.eventTimestamp, -123456)
+        XCTAssertEqual(date3.eventTimestamp, Int(date3.timeIntervalSince1970))
+        XCTAssertEqual(date4.eventTimestamp, 123456)
+        XCTAssertEqual(date4, date1)
+    }
+    
+    func testEventDecodable() {
+        // 1. given
+        let id = "abc"
+        let version = 3
+        let type = 2
+        let timestamp = 123456
+        let foo = "bar"
+        let baz = 123
+        
+        let baseJson = """
+{
+    "_i": "\(id)",
+    "_v": \(version),
+    "_e": \(type),
+    "_t": \(timestamp),
+        "foo": "\(foo)",
+        "baz": \(baz),
+        "null": null,
+        "arr": [1, 2, 3],
+        "obj": {"a": 1, "b": 2, "c": 3}
+}
+"""
+        let emptyJson = """
+{}
+"""
+        let corruptTypeJson = """
+{
+    "_i": 123,
+    "_v": "3",
+    "_e": "1",
+    "_t": false
+}
+"""
+        // 2. when
+        XCTAssertThrowsError(try JSONDecoder().decode(Event.self, from: emptyJson.data(using: .utf8)!))
+        XCTAssertThrowsError(try JSONDecoder().decode(Event.self, from: corruptTypeJson.data(using: .utf8)!))
+        XCTAssertNoThrow(try JSONDecoder().decode(Event.self, from: baseJson.data(using: .utf8)!))
+        let baseEvent: Event = try! JSONDecoder().decode(Event.self, from: baseJson.data(using: .utf8)!)
+        
+        // 3. then
+        XCTAssertEqual(baseEvent.id.rawValue, id)
+        XCTAssertEqual(baseEvent.version, version)
+        XCTAssertEqual(baseEvent.type, type)
+        XCTAssertEqual(baseEvent.timestamp.eventTimestamp, timestamp)
+        XCTAssertEqual(baseEvent.payload, ["foo": .string(foo),
+                                           "baz": .int(baz),
+                                           "null": .null,
+                                           "arr": .array([.int(1), .int(2), .int(3)]),
+                                           "obj": .object(["a": .int(1), "b": .int(2), "c": .int(3)])] as [String: JSONValue])
+    }
+    
+    func testEventEncodable() {
+        // 1. given
+        let event = Event(id: Event.Identifier(rawValue: "abc123"), version: 3, timestamp: Date(), type: 123, payload: ["a": .string("foo"), "b": .int(123), "c": .bool(false), "d": .null])
+        
+        // 2. when
+        
+        XCTAssertNoThrow(try JSONEncoder().encode(event))
+        
+        let jsonData = try! JSONEncoder().encode(event)
+        let decodedEvent = try! JSONDecoder().decode(Event.self, from: jsonData)
+        
+        // 3. then
+        XCTAssertEqual(decodedEvent, event)
+    }
+}
