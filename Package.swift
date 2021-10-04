b1dc8d55fd92aa2bff706f44b9270548ae8ea542
Laurie Hufford <lauriehufford@gmail.com>
2021-10-04 14:38:26 +0200

    Allow SPM version to work in XCode 13. Use the latest version of Kingfisher, and bump to requiring iOS 12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "ShopGunSDK",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v12)
     ],
     products: [
         .library(
@@ -22,7 +22,7 @@ let package = Package(
         .package(name: "Incito", url: "https://github.com/shopgun/incito-ios.git", from: "1.0.3"),
         .package(name: "Future", url: "https://github.com/shopgun/swift-future.git", from: "0.5.0"),
         .package(name: "Verso", url: "https://github.com/shopgun/verso-ios.git", from: "1.0.5"),
-        .package(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher.git", from: "6.0.1"),
+        .package(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
         .package(name: "Valet", url: "https://github.com/square/Valet.git", from: "4.1.1")
     ],
     targets: [
