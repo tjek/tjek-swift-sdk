be953bcbc573aa480da53caf1869167dbee97f34
Laurie Hufford <lauriehufford@gmail.com>
2021-11-03 10:07:34 +0100

    Update headers

--- Sources/TjekPublicationReader/PagedPublication/PagedPublicationView+ProtocolDefaults.swift
+++ Sources/TjekPublicationReader/PagedPublication/PagedPublicationView+ProtocolDefaults.swift
@@ -1,11 +1,6 @@
-//
-//  ┌────┬─┐         ┌─────┐
-//  │  ──┤ └─┬───┬───┤  ┌──┼─┬─┬───┐
-//  ├──  │ ╷ │ · │ · │  ╵  │ ╵ │ ╷ │
-//  └────┴─┴─┴───┤ ┌─┴─────┴───┴─┴─┘
-//               └─┘
-//
-//  Copyright (c) 2018 ShopGun. All rights reserved.
+///
+///  Copyright (c) 2018 Tjek. All rights reserved.
+///
 
 import UIKit
 
