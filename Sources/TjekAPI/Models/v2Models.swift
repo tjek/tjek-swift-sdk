e500a6d41f20406a3a87fc86621b62442ecba8c8
Laurie Hufford <lauriehufford@gmail.com>
2022-02-15 09:17:33 +0100

    Allow v2 offer price to be encodable

--- Sources/TjekAPI/Models/v2Models.swift
+++ Sources/TjekAPI/Models/v2Models.swift
@@ -250,7 +250,7 @@ extension Offer_v2: Decodable {
 
 extension Offer_v2 {
     
-    public struct Price: Decodable, Equatable {
+    public struct Price: Codable, Equatable {
         public var currency: String
         public var price: Double
         public var prePrice: Double?
