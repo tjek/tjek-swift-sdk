57749a7960a2e9aab52d6cda4860fb6d63d9fefa
Laurie Hufford <lauriehufford@gmail.com>
2022-10-04 11:32:35 +0200

    Hopefully fixes GH actions complaining

--- Sources/TjekAPI/APIClient/APIClient.swift
+++ Sources/TjekAPI/APIClient/APIClient.swift
@@ -146,3 +146,22 @@ extension APIRequestSender {
 }
 
 #endif
+
+// We are getting errors in the GH actions because it is trying to use the iOS 15 implementation of this function.
+@available(iOS, deprecated: 15.0, message: "Use the built-in API instead")
+extension URLSession {
+    fileprivate func data(from request: URLRequest) async throws -> (Data, URLResponse) {
+         try await withCheckedThrowingContinuation { continuation in
+             let task = self.dataTask(with: request, completionHandler: { data, response, error in
+                 guard let data = data, let response = response else {
+                     let error = error ?? URLError(.badServerResponse)
+                     return continuation.resume(throwing: error)
+                 }
+
+                 continuation.resume(returning: (data, response))
+             })
+
+             task.resume()
+        }
+    }
+}
