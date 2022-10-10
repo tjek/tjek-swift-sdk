a762ad506990c79c23fe48f13ee46d09fd11ec6e
Laurie Hufford <lauriehufford@gmail.com>
2022-10-10 10:05:25 +0200

    Allow APIRequest maps to be async

--- Sources/TjekAPI/APIClient/APIRequest.swift
+++ Sources/TjekAPI/APIClient/APIRequest.swift
@@ -65,7 +65,7 @@ public struct APIRequest<ResponseType> {
 extension APIRequest {
     /// Converts an APIRequest of one responseType into an APIRequest of a different type, by modifying the decoder.
     public func map<NewResponseType>(
-        _ transform: @escaping (ResponseType) -> NewResponseType
+        _ transform: @escaping (ResponseType) async throws -> NewResponseType
     ) -> APIRequest<NewResponseType> {
         APIRequest<NewResponseType>(
             path: self.path,
